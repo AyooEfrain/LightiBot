c1a6bcd14027b9b277b197410b86049bdfa1e590
AyooEfrain <45703381+AyooEfrain@users.noreply.github.com>
2025-03-15 13:05:43 -0500

    Testing command work proccess / test joke cmd

--- src/main/kotlin/extensions/joke/JokeExtension.kt
+++ src/main/kotlin/extensions/joke/JokeExtension.kt
@@ -1,9 +1,12 @@
 package extensions.joke
 
-import com.kotlindiscord.kord.extensions.extensions.Extension
-import com.kotlindiscord.kord.extensions.commands.application.slash.ephemeralSlashCommand
 import com.kotlindiscord.kord.extensions.checks.hasPermission
 import com.kotlindiscord.kord.extensions.checks.isNotBot
+import com.kotlindiscord.kord.extensions.extensions.Extension
+import com.kotlindiscord.kord.extensions.modules.unsafe.annotations.UnsafeAPI
+import com.kotlindiscord.kord.extensions.modules.unsafe.extensions.unsafeSlashCommand
+import com.kotlindiscord.kord.extensions.modules.unsafe.types.InitialSlashCommandResponse
+import com.kotlindiscord.kord.extensions.modules.unsafe.types.respondPublic
 import dev.kord.common.entity.Permission
 
 class JokeExtension : Extension() {
@@ -12,13 +15,16 @@ class JokeExtension : Extension() {
     private val jokes = listOf(
         "Why did the developer go broke? Because he used up all his cache!",
         "Why do programmers prefer dark mode? Because light attracts bugs!",
-        "What do you call a programmer from Finland? Nerdic!"
+        "What do you call a programmer from Finland? Nerdic!",
+        "Why is deucecan the way the he is? Because he is a deucecan!"
     )
 
+    @OptIn(UnsafeAPI::class)
     override suspend fun setup() {
-        ephemeralSlashCommand {
+        unsafeSlashCommand {
             name = "joke"
             description = "Get a random programming joke"
+            initialResponse = InitialSlashCommandResponse.None
 
             check {
                 hasPermission(Permission.SendMessages)
@@ -26,7 +32,7 @@ class JokeExtension : Extension() {
             }
 
             action {
-                respond {
+                respondPublic {
                     content = jokes.random()
                 }
             }
